90b92e9b8c58740d6a3301b65f7a981643ec22f9
Joel <vashappnin107@gmail.com>
2024-07-01 13:59:43 +0530

    first commit

---
+++ Desktop/python/loop.py
@@ -0,0 +1,9 @@
+i = int(input('enter ur number '))
+if i > 10:
+    i = int(input('enter a num less than 10'))
+else:
+    a=0
+    while a< 10:
+     a= a+1
+     print(i,'x',a,'=',i*a)
+    
---
+++ Desktop/python/nestedifelse.py
@@ -0,0 +1,8 @@
+num = float (input('enter a num = '))
+if num >= 0:
+    if num == 0:
+        print('Zero')
+    else:
+        print('positive num')
+else:
+    print('neg')
